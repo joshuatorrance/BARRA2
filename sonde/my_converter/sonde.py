adaacf779adc3ef44c431f270298c8516c533470
Joshua Torrance <joshua.torrance@bom.gov.au>
2022-03-09 14:08:23 +1100

    Renamed b_temp to output_bufr

--- sonde/my_converter/sonde.py
+++ sonde/my_converter/sonde.py
@@ -276,24 +276,24 @@ class SondeBUFR:
             self.wind_speed[i] = '#' + str(i + 1) + '#windSpeed'
 
         with open(template_path, 'r') as f_template:
-            self.b_temp = ecc.codes_bufr_new_from_file(f_template)
+            self.output_bufr = ecc.codes_bufr_new_from_file(f_template)
 
         self._year_month_day_index = 0
 
-        ecc.codes_set(self.b_temp, 'unpack', 1)
+        ecc.codes_set(self.output_bufr, 'unpack', 1)
 
         # Fixed header values, do this once
-        ecc.codes_set(self.b_temp, 'edition', 3)
-        ecc.codes_set(self.b_temp, 'masterTableNumber', 0)
-        ecc.codes_set(self.b_temp, 'bufrHeaderCentre', 1)
-        ecc.codes_set(self.b_temp, 'dataCategory', 2)
-        ecc.codes_set(self.b_temp, 'dataSubCategory', 109)  # land temp
-        ecc.codes_set(self.b_temp, 'masterTablesVersionNumber', 26)
-        ecc.codes_set(self.b_temp, 'localTablesVersionNumber', 0)
+        ecc.codes_set(self.output_bufr, 'edition', 3)
+        ecc.codes_set(self.output_bufr, 'masterTableNumber', 0)
+        ecc.codes_set(self.output_bufr, 'bufrHeaderCentre', 1)
+        ecc.codes_set(self.output_bufr, 'dataCategory', 2)
+        ecc.codes_set(self.output_bufr, 'dataSubCategory', 109)  # land temp
+        ecc.codes_set(self.output_bufr, 'masterTablesVersionNumber', 26)
+        ecc.codes_set(self.output_bufr, 'localTablesVersionNumber', 0)
 
-        ecc.codes_set(self.b_temp, 'typicalMinute', 0)
-        ecc.codes_set(self.b_temp, 'compressedData', 1)
-        ecc.codes_set(self.b_temp, 'numberOfSubsets', 1)
+        ecc.codes_set(self.output_bufr, 'typicalMinute', 0)
+        ecc.codes_set(self.output_bufr, 'compressedData', 1)
+        ecc.codes_set(self.output_bufr, 'numberOfSubsets', 1)
 
     def t_bias(self, hour_index, sonde_txt, sonde_nc):
         """
@@ -304,7 +304,7 @@ class SondeBUFR:
                 if sonde_nc.pressure[nc_level_index] == sonde_txt.pressure[txt_level_index]:
                     if sonde_nc.air_temp[hour_index, nc_level_index, self._year_month_day_index] != sonde_nc.MISSING \
                           and sonde_nc.bias[hour_index, nc_level_index, self._year_month_day_index] != sonde_nc.MISSING:
-                        ecc.codes_set(self.b_temp,
+                        ecc.codes_set(self.output_bufr,
                                       self.air_temp[txt_level_index],
                                       np.float64(sonde_nc.air_temp[hour_index,
                                                                    nc_level_index,
@@ -319,40 +319,40 @@ class SondeBUFR:
         write out sonde for barra2
         """
 
-        ecc.codes_set(self.b_temp, 'unpack', 1)
+        ecc.codes_set(self.output_bufr, 'unpack', 1)
 
         # TODO: 0.01 * year? What's going on here?
         #  2022 * 0.01 = 2022 / 100 = 20.22... a float and not the "typicalYearOfCentury"
         #  I suspect it should be (year % 100) to get two digit year,
         #  or just get get the two digit year from the datetime
-        ecc.codes_set(self.b_temp, 'typicalYearOfCentury', 0.01 * sonde_txt_obs.date_time.year)
-        ecc.codes_set(self.b_temp, 'typicalMonth', sonde_txt_obs.date_time.month)
-        ecc.codes_set(self.b_temp, 'typicalDay', sonde_txt_obs.date_time.day)
-        ecc.codes_set(self.b_temp, 'typicalHour', sonde_txt_obs.date_time.hour)
+        ecc.codes_set(self.output_bufr, 'typicalYearOfCentury', 0.01 * sonde_txt_obs.date_time.year)
+        ecc.codes_set(self.output_bufr, 'typicalMonth', sonde_txt_obs.date_time.month)
+        ecc.codes_set(self.output_bufr, 'typicalDay', sonde_txt_obs.date_time.day)
+        ecc.codes_set(self.output_bufr, 'typicalHour', sonde_txt_obs.date_time.hour)
 
-        ecc.codes_set(self.b_temp,
+        ecc.codes_set(self.output_bufr,
                       'inputExtendedDelayedDescriptorReplicationFactor', sonde_txt_obs.n_levels)
-        ecc.codes_set_array(self.b_temp, 'unexpandedDescriptors', SondeBUFR.TEMP_SEQ)
+        ecc.codes_set_array(self.output_bufr, 'unexpandedDescriptors', SondeBUFR.TEMP_SEQ)
 
         # ecc.codes_set(self.b_temp, 'shipOrMobileLandStationIdentifier', 'ASM000')
-        ecc.codes_set(self.b_temp, 'blockNumber', sonde_txt_obs.station_blk)
-        ecc.codes_set(self.b_temp, 'stationNumber', sonde_txt_obs.station_number)
-        ecc.codes_set(self.b_temp, 'year', sonde_txt_obs.date_time.year)
-        ecc.codes_set(self.b_temp, 'month', sonde_txt_obs.date_time.month)
-        ecc.codes_set(self.b_temp, 'day', sonde_txt_obs.date_time.day)
-        ecc.codes_set(self.b_temp, 'hour', sonde_txt_obs.date_time.hour)
-        ecc.codes_set(self.b_temp, 'minute', 0)
-        ecc.codes_set(self.b_temp, 'latitude', sonde_txt_obs.lat)
-        ecc.codes_set(self.b_temp, 'longitude', sonde_txt_obs.lon)
-        ecc.codes_set(self.b_temp, 'heightOfStation', sonde_txt_obs.station_height)
+        ecc.codes_set(self.output_bufr, 'blockNumber', sonde_txt_obs.station_blk)
+        ecc.codes_set(self.output_bufr, 'stationNumber', sonde_txt_obs.station_number)
+        ecc.codes_set(self.output_bufr, 'year', sonde_txt_obs.date_time.year)
+        ecc.codes_set(self.output_bufr, 'month', sonde_txt_obs.date_time.month)
+        ecc.codes_set(self.output_bufr, 'day', sonde_txt_obs.date_time.day)
+        ecc.codes_set(self.output_bufr, 'hour', sonde_txt_obs.date_time.hour)
+        ecc.codes_set(self.output_bufr, 'minute', 0)
+        ecc.codes_set(self.output_bufr, 'latitude', sonde_txt_obs.lat)
+        ecc.codes_set(self.output_bufr, 'longitude', sonde_txt_obs.lon)
+        ecc.codes_set(self.output_bufr, 'heightOfStation', sonde_txt_obs.station_height)
 
         for i in range(sonde_txt_obs.n_levels):
-            ecc.codes_set(self.b_temp, self.pressure[i], sonde_txt_obs.pressure[i])
-            ecc.codes_set(self.b_temp, self.height[i], sonde_txt_obs.height[i])
-            ecc.codes_set(self.b_temp, self.air_temp[i], sonde_txt_obs.air_temp[i])
-            ecc.codes_set(self.b_temp, self.dew_point_temp[i], sonde_txt_obs.dew_point_temp[i])
-            ecc.codes_set(self.b_temp, self.wind_direction[i], sonde_txt_obs.wind_direction[i])
-            ecc.codes_set(self.b_temp, self.wind_speed[i], sonde_txt_obs.wind_speed[i])
+            ecc.codes_set(self.output_bufr, self.pressure[i], sonde_txt_obs.pressure[i])
+            ecc.codes_set(self.output_bufr, self.height[i], sonde_txt_obs.height[i])
+            ecc.codes_set(self.output_bufr, self.air_temp[i], sonde_txt_obs.air_temp[i])
+            ecc.codes_set(self.output_bufr, self.dew_point_temp[i], sonde_txt_obs.dew_point_temp[i])
+            ecc.codes_set(self.output_bufr, self.wind_direction[i], sonde_txt_obs.wind_direction[i])
+            ecc.codes_set(self.output_bufr, self.wind_speed[i], sonde_txt_obs.wind_speed[i])
 
         # ecc.codes_set(self.b_temp, 'radiosondeType', t.sonde_type)
 
@@ -377,8 +377,8 @@ class SondeBUFR:
                 self._year_month_day_index += 1
 
         # Avoid unpacking self.b_temp the next time
-        ecc.codes_set(self.b_temp, 'pack', 1)
-        ecc.codes_write(self.b_temp, file_bufr)
+        ecc.codes_set(self.output_bufr, 'pack', 1)
+        ecc.codes_write(self.output_bufr, file_bufr)
 
     def close(self):
-        ecc.codes_release(self.b_temp)
+        ecc.codes_release(self.output_bufr)
