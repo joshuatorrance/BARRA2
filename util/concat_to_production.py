c76d154a14dd6ff9ebb83c485961dcb4b2ae0ae6
Joshua Torrance <joshua.torrance@bom.gov.au>
2022-04-04 14:54:06 +1000

    Added script to concatenate multiple bufrs and output to production dirs

---
+++ util/concat_to_production.py
@@ -0,0 +1,82 @@
+# This script takes files from one archive, concatenate the files within a bin
+# if necessary and moves the result to the production archive, creating a
+# symlink for the BARRA2 suite to use.
+
+## IMPORTS
+from glob import glob
+from os import makedirs, symlink, remove
+from os.path import join, basename, normpath, exists
+from shutil import move
+
+
+## PARAMETERS
+INPUT_DIR = "/g/data/hd50/barra2/data/obs/amsr2"
+OUTPUT_DIR = "/g/data/hd50/barra2/data/obs/production"
+OUTPUT_DIR = "/scratch/hd50/jt4085/production_test"
+
+TYPE = "amsr"
+
+OUTPUT_FILENAME = TYPE + "_{dt_str}.bufr"
+TEMP_SUFFIX = ".temp"
+
+SYMLINK_FILENAME = "AMSR2_1.bufr"
+
+
+## SCRIPT
+def main():
+
+    for y_dir in glob(join(INPUT_DIR, "*")):
+        y = basename(y_dir)
+
+        print("Year:", y)
+
+        for m_dir in glob(join(y_dir, "*")):
+            m = basename(m_dir)
+
+            print("\tMonth:", m)
+
+            for dt_dir in glob(join(m_dir, "*")):
+                dt = basename(dt_dir)
+
+                print("\t\t", dt)
+
+                # Find the corresponding output dir
+                out_dir = join(OUTPUT_DIR, y, m, dt, "bufr", TYPE)
+
+                # If the output directory doesn't exist create it.
+                makedirs(out_dir, exist_ok=True)
+
+                # Build the output file paths
+                out_filename = OUTPUT_FILENAME.format(dt_str=dt)
+                out_filepath = join(out_dir, out_filename)
+
+                if exists(out_filpath):
+                    print("\t\t\tOutput file already exists skipping")
+                    continue
+
+                temp_out_filepath = out_filepath + TEMP_SUFFIX
+
+                # Concatenate the input files into a temp single file
+                with open(temp_out_filepath, 'wb') as temp_out_file:
+                    for f in glob(join(dt_dir, "*.bufr")):
+                        f_name = basename(f)
+
+                        with open(f, 'rb') as in_file:
+                            temp_out_file.write(in_file.read())
+
+                # Move the temp file to the output file
+                move(temp_out_filepath, out_filepath)
+
+                # Create a symlink to the outfile
+                symlink_filepath = join(out_dir, SYMLINK_FILENAME)
+
+                if exists(symlink_filepath):
+                    # Delete the symlink if it already exists
+                    remove(symlink_filepath)
+
+                symlink(out_filepath, symlink_filepath)
+
+
+if __name__ == "__main__":
+    main()
+
